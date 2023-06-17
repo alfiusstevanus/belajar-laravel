728780e4ce218d066d68ad4758da5332055ac138
alfiusstevanus <alfiusstevanus18@gmail.com>
2023-06-17 20:53:08 +0700

    belajar laravel hari ke 2 beres

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\DashboardController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
-use App\Models\Post;
-use App\Models\User;
-
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -48,3 +49,12 @@ Route::get('/categories', function () {
         'categories' => Category::all()
     ]);
 });
+
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
